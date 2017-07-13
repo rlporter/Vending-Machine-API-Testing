31d9f1e65d3e544c130a1d9e4cf291d323ac4594
rlporter <rebekkah1@me.com>
2017-07-12 23:17:56 -0400

    updates

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
-
 const models = require('./models');
+
+app.get('/', function (req, res){
+  res.json({});
+});
 
 if (require.main === 'module') {
   app.listen(3000, function () {
-      console.log('Express running on http://localhost:3000/.')
+      console.log('Express running on http://localhost:3000/.');
   });
 }
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,23 @@
+const request = require('supertest');
 const assert = require('assert');
+const app = require('../app');
 const config = require('../config')[process.env.NODE || 'test'];
 const items = require('../models/items');
+
+before('connect to sequelize', function(done) {
+  const sequelize = new Sequelize('postgres://user:pass@example.com:5432/vendingMachine').then(done);
+});
+
+after('drop database', function (done){
+  sequelize.close(done);
+});
+
+describe('GET /customer/items', function(){
+  it('should return successfully', function (done){
+    request(app)
+    .get('/customer/items')
+    .expect(200)
+
+  })
+  .end(done);
+})
